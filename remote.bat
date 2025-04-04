587923874971ad4a20ef5d31bdd8f2464c5984ee
Dhagar <127939440+DhagarRc@users.noreply.github.com>
2025-04-04 09:28:59 +0200

    Update remote.bat

--- remote.bat
+++ remote.bat
@@ -1,5 +1,1 @@
-echo start msedge --new-window https://eaglercraft.com/mc/1.12.2/ > "%USERPROFILE%\Desktop\Minecraft-Classic.bat"
 
-curl -o "%~dp0file.jpg" "https://images.steamusercontent.com/ugc/2453988158187360115/0F88D6CE106367B972ED8997ECA4AA351426481C/"
-REG ADD "HKCU\Control Panel\Desktop" /V Wallpaper /T REG_SZ /F /D "%~dp0file.jpg"
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
